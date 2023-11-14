45fe851b324b939d4661803c8a31c23fee8d9aca
Meron Edea <edeameron@gmail.com>
2023-11-14 07:01:35 -0900

    string to token split

--- shell.h
+++ shell.h
@@ -13,13 +13,15 @@
 
 /********CONSTANTS*******/
 #define BUFFER_SIZE 1024
+#define MAX_INPUT_SIZE 1024
 
 /******PROTOTYPES******/
-
 void show_prompt(void);
 void execute(const char *command);
 void _print(char *format);
 void _read(char *command, size_t size);
 void shell_loop(void);
 char read_line(void);
+void string_token(const char *input, char *command, char *arguments);
+
 #endif
