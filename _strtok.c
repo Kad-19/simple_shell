8271a8087fa6a6c08d4050962b620a71638a6a96
Meron Edea <edeameron@gmail.com>
2023-11-15 10:49:42 -0900

    betty check

--- _strtok.c
+++ _strtok.c
@@ -1,10 +1,19 @@
 #include "shell.h"
-
+/**
+ * stringToken - a function that splits string to tokens
+ * @input: The input string to be tokenized.
+ * @command: Pointer to a character array to store the command token.
+ * @arguments: Pointer to a character array to store the arguments token.
+ * 
+ * This function takes an input string and splits it into tokens, separating the command and arguments.
+ * 
+ * Return: error message
+ */
 void stringToken(const char *input, char *command, char *arguments)
 {
     int i = 0;
     int j = 0;
-
+    
     while (input[i] != ' ' && input[i] != '\0')
     {
         command[j] = input[i];
--- get_line.c
+++ get_line.c
@@ -1,5 +1,15 @@
 #include "shell.h"
-
+/**
+ * readLine - Reads a line of input from the standard input.
+ *
+ * This function reads a line of input from the standard input, character by character,
+ * until it encounters a newline character ('\n') or reaches the end of the input.
+ * It dynamically allocates memory to store the input line and expands the memory as needed.
+ *
+ * Return: A pointer to a dynamically allocated string containing the input line,
+ *         or NULL if there is no more input or an error occurs.
+ *
+ */
 char *readLine(void)
 {
     static char buffer[BUFFER_SIZE];
